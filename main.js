5209027491fa5af23e500b0c611d6e030d2023da
Pim Blom <pim.blom@icloud.com>
2023-03-06 23:11:48 +0100

    keys displayed + random sequence

--- main.js
+++ main.js
@@ -1,40 +1,107 @@
+class Game{
+  constructor() {
+    this.createDisplay()
+    this.countLevel=0
+
+    
+}
+createDisplay() {
+  // step1: create the element
+  const displayElm = document.createElement("div");
+  // step2: add content (ex. innerText) and/or modify attributes
+  displayElm.setAttribute("id","newKeys")
+  //step3: append to the dom
+  const boardElm = document.getElementById("board");
+  boardElm.appendChild(displayElm);
+}
+
+countLevel(){
+  this.countLevel++
+  console.log("level" + this.countLevel)
+}
+
+}
+
+
 class Keys {
   constructor() {
+    this.newArr = [];
     this.newSequenceArr = [];
     this.keysPressed = [];
-    this.keys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+    this.keys= [
+      {name: "ArrowLeft", image:"<"},
+      {name: "ArrowRight", image: ">"},
+      {name: "ArrowUp", image: "^"},
+      {name: "ArrowDown", image: "v"}
+    ]
 
-    createNewSequence();
+    
+    this.createNewSequence();
+    
   }
   createNewSequence() {
-    keys.forEach((element) => {
-      newSequenceArr.push(element);
-      console.log(newSequenceArr)
-    });
+    
+    for (let i = 0; i < 5; i++) {
+      let random=(Math.floor(Math.random()*4))
+      this.newSequenceArr.push(this.keys[random])
   }
+        this.newArr= this.newSequenceArr.map(key => key.name)
+        console.log (this.newArr)
+        this.showNewSequence()
+        return this.newArr
+}
+
+    
+  
+
+  showNewSequence(){
+    let showElm= document.getElementById("newKeys");
+    
+    for(let i=0;i<this.newArr.length;i++){ 
+      setTimeout(()=>{showElm.innerHTML=this.newArr[i]
+      setTimeout(()=>{showElm.innerHTML=""},850)},i*1000)
+      }
+      {this.attachEventListener()}
+    }
+    /*this.newSequenceArr.forEach((elm, index)=>{
+      setTimeout(()=>{showElm.innerHTML=elm}
+      ,index*1000)
+  
+    })*/
+    
+  
 
   attachEventListener(){
     document.addEventListener("keydown", (event) => {
-  keysPressed.push(event.key);
-  console.log(keysPressed);
+    this.keysPressed.push(event.key);
+    console.log(this.keysPressed);
+    this.compareInputAndArr();
+    return this.keysPressed
     })
 }
 
 compareInputAndArr() {
   setTimeout(() => {
-    if (keysPressed.toString() === newSequenceArr.toString()) {
+    if (this.keysPressed.toString() === this.newArr.toString()) {
       console.log("good job");
+      
+      this.newArr.splice(0,this.newArr.length)
+      this.newSequenceArr.splice(0,this.newSequenceArr.length)
+      this.createNewSequence()
     } else console.log("game over");
     //console.log(newSequenceArr);
     //console.log(keysPressed);
-  }, 5000);
+  }, 10 * 1000);
 }
 
+
 }
 
 
-compareInputAndArr();
 
+
+
+const newGame = new Game()
 const newKeys = new Keys()
 
 
@@ -51,13 +118,15 @@ const newKeys = new Keys()
 //setTimeout(() => {
 //    }, timeout);
 
-function createNewSequence() {
-  // step1: create the element
-  this.keysElm = document.createElement("div p");
-  // step2: add content (ex. innerText) and/or modify attributes
-  this.keysElm.className = "newKeys";
-  this.keysElm.innerText = "<=";
-  //step3: append to the dom
-  const boardElm = document.getElementById("board");
-  boardElm.appendChild(this.keysElm);
-}*/
+
+function showNewArr(){
+        // step1: create the element
+    keysElm = document.createElement("div p");
+        // step2: add content (ex. innerText) and/or modify attributes
+    keysElm.className = "newSequenceArr";
+    keysElm.innerText= newSequenceArr
+        //step3: append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(keysElm);
+      }
+*/
